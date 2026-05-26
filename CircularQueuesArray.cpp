209f76b88406a3fc83b2bec1059ca2f19b91fbdc
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-26 11:31:48 +0700

    mengecek antrian

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -40,7 +40,22 @@ private:
                 else
                 REAR = REAR + 1;
             }
+
             queue_array[REAR] = num;
         }
 
-        
+        void remove()
+        {
+            if (FRONT == -1)
+            {
+                cout << "Queue underflow\n";
+                return;
+            }
+            cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+            if (FRONT == REAR)
+            {
+                FRONT = -1;
+                REAR = -1;
+            }
+            
