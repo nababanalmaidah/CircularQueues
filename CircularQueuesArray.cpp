33bfa1787686996d00e31f90e57384d8c7ca6e04
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-26 11:36:56 +0700

    insiaLISASI awal program circular

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -89,4 +89,17 @@ private:
         FRONT_Position++;
     }
 
+    FRONT_Position = 0;
+
+    while (FRONT_Position <= REAR_Position)
+    {
+        cout << queue_array[FRONT_Position] << " ";
+        FRONT_Position++;
+    }
+
+    cout << endl;
+}
+else
+{
+
     
