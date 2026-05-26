ffa9862e217c265f586fdf16d8037a5e9de4b96b
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-26 11:29:42 +0700

    membuat struktur dasar circular queue array

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -28,4 +28,19 @@ private:
                  return;
             }
 
-            
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                if (REAR == max - 1)
+                   REAR = 0;
+                else
+                REAR = REAR + 1;
+            }
+            queue_array[REAR] = num;
+        }
+
+        
