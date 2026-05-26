694c6cd8a7d68d449f6f7f7b6d5a3ed95c723a1f
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-26 11:38:08 +0700

    menambahkan nprogram identias  program cirlcular

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -102,4 +102,16 @@ private:
 else
 {
 
-    
+    while (FRONT_Position <= MAX - 1)
+    {
+           cout << queue_array[FRONT_Position] << " ";
+           FRONT_Position++;
+   } 
+
+   FRONT_Position = 0;
+
+   while (FRONT_Position <= REAR_Position)
+   {
+        cout << queue_array[FRONT_Position] << " ";
+        FRONT_Position++;
+}
